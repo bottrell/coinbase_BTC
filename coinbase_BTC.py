0167ea4a690c4f472270f3427af6e9f3843f6d4f
Jordan Bottrell <bottrell@umich.edu>
2018-02-06 19:40:28 -0500

    added stub for sell and trade

--- coinbase_BTC.py
+++ coinbase_BTC.py
@@ -15,29 +15,58 @@ def get_price(client):
 	price = client.get_spot_price(currency = currency_code)
 	return price.amount
 
-def limit_buy(client, account, current_price_BTC):
+# added a limit parameter, so we never have a static limit_buy setting,
+# that way we can be sure to not get stuck in a buy loop if a limit
+# gets set at a super low threshold.
+def limit_buy(client, account, current_price_BTC, limit):
 	#get the primary account
 	btc_wallet = client.get_primary_account()
 	#let's not add a payment method yet so we dont end up
 	#accidentally buying coins lol
-	buy_price_threshold = 0
+	buy_price_threshold = limit
 	#if the current price is meets our threshold, buy 1 satoshi
 	if float(current_price_BTC) <= buy_price_threshold:
 		buy = account.buy(amount = '.000001',
 						  currency = 'BTC',
 						  payment_method = payment_method.id)
 
-def current_profit(client, account, current_price_BTC):
+# Pass in peak so the function doesn't get stuck in a loop of selling
+# all of my bitcoins on any sort of price peak
+def sell_high(client, account, current_price_BTC, peak):
+	pass
+
+
+def current_profit(client, account, current_price_BTC, account_balance):
 	transactions = account.get_transactions()
 	price_bought = 0.0
-	price_sold = 0.0
-	amount_profit = 0.0
+	price_after_fees = 0.0
 	for transaction in transactions["data"]:
 		if(transaction["type"] == "buy"):
-			price_bought += float(transaction["native_amount"]["amount"])
-			print(price_bought)
+			if(transaction["details"]["payment_method_name"][0] == "M"):
+				price_bought += float(transaction["native_amount"]["amount"])
+				amount = float(transaction["native_amount"]["amount"]) - (.04 * float(transaction["native_amount"]["amount"]))
+				price_after_fees += amount
+				#print(transaction)
+			else:
+				price_bought += float(transaction["native_amount"]["amount"])
+				amount = float(transaction["native_amount"]["amount"]) - (.01 * float(transaction["native_amount"]["amount"]))
+				price_after_fees += amount
+				#print(transaction)
+
+	amount_payed_fees = round((price_bought - price_after_fees), 2)
+	print("Amount payed in fees: " + str(amount_payed_fees))
+	profit = round((account_balance - price_after_fees), 2)
+	print("Total profit to this point: %s" % profit)
+	return profit
 		#print(transaction, end = "\n\n\n\n\n\n\n\n\n")
 
+#-------------------------------------------------------------#
+#
+# DEFINITELY TEST THE HECK OUT OF THIS AS TO NOT LOSE MONEY
+#
+#-------------------------------------------------------------#
+def trade(client, account, account_balance):
+	pass
 ##################################################################
 
 def main():
@@ -46,7 +75,7 @@ def main():
 	#balance = account.balance
 	#print("%s: %s %s" % (account.name, balance.amount, balance.currency))
 	#print(account.get_transactions())
-	print("Starting coinbase...")
+	print("\n\nSTARTING COINBASE...", end = "\n\n\n")
 	
 	client = Client(
 	secrets.API_KEY,
@@ -61,10 +90,13 @@ def main():
 	primary_account.refresh()
 	balance_BTC = primary_account.balance
 	balance_USD = float(current_price) * float(balance_BTC.amount)
+	balance_USD = round(balance_USD, 2)
 	print("{}: {} {}".format(primary_account.name, balance_BTC.amount, balance_BTC.currency))
 	print("Your wallet value is: {}".format(balance_USD))
 
-	current_profit(client, primary_account, current_price)
+	current_profit(client, primary_account, current_price, balance_USD)
+
+	print("\n\n\nBUYING AND SELLING BTC BASED ON METRICS FROM PAST WEEK...\n\n\n")
 
 
 if __name__ == main():
